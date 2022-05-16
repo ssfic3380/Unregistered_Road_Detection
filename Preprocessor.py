4b30904bf70c03c642fd3eb78091f134439feba4
Taeyeong <htyvv@naver.com>
2022-05-16 22:37:11 +0900

    preprocessor done

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,12 +1,15 @@
+import pandas as pd
 import numpy as np
 import tensorflow as tf
 
 class Preprocessor:
-    def __init__(self):
+    def __init__(self, dataset:pd.DataFrame):
+        self.dataset = dataset,
         self.random_seed = 10
         
     def set_random_seed(self) -> None:
         # set random seed
         np.random.seed(self.random_seed)
         tf.random.set_seed(self.random_seed)
+    
     
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import Preprocessor
+from data_loader import Data_loader
+from preprocessor import Preprocessor
 
 if __name__ == "__main__":
+    
+    # 데이터 불러오기
+    loader = Data_loader()
+    df_list = loader.load_datasets()
+    
+    # 전처리 하기
     preprocessor = Preprocessor()
+    preprocessor.set_random_seed()
