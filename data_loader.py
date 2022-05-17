f873d1cc0e794aca8bd91d755541743e8a92f941
Taeyeong <htyvv@naver.com>
2022-05-17 20:01:36 +0900

    Change variable names

--- data_loader.py
+++ data_loader.py
@@ -7,15 +7,15 @@ class Data_loader:
     def __init__(self):
         self.log_segment_info_path = '../prev_src/log_segment_info/'
         self.use_cols = ['mercX', 'mercY']
-        self.users = ['0mnEB226qqgHE79KLEfxRj6fiEK2',
-                      '9z6F6ewGNzV4a6z0vpenjZH21Ar1',
-                     ]
+        self.users_hash = ['0mnEB226qqgHE79KLEfxRj6fiEK2',
+                            '9z6F6ewGNzV4a6z0vpenjZH21Ar1',
+                            ]
         
     def load_datasets(self) -> List[pd.DataFrame]:
         data_list = []
         
-        for user in self.users:
-            files_dir = self.log_segment_info_path + user
+        for user_hash in self.users_hash:
+            files_dir = self.log_segment_info_path + user_hash
             for file_name in listdir(files_dir):
        
                 # log 파일이 아닐 경우 continue
@@ -23,12 +23,13 @@ class Data_loader:
                     continue
                 
                 # log 파일 load
-                file_dir = "/".join([files_dir, file_name])
+                file_path = "/".join([files_dir, file_name])
+                file_path = f"{files_dir}/{file_name}"
                 try:
-                    file_df = pd.read_csv(file_dir, usecols=self.use_cols)
+                    file_df = pd.read_csv(file_path, usecols=self.use_cols)
                     # log의 길이가 0이면 스킵
                     if len(file_df) == 0:
-                        print(f"❗ Warning: [{file_dir}]'s log length is zero")
+                        print(f"❗ Warning: [{file_path}]'s log length is zero")
                         continue
                     data_list.append(file_df)
                 except Exception as error_message:
--- main.py
+++ main.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     # 데이터 불러오기
     loader = Data_loader()
     df_list = loader.load_datasets()
+    print(f"✅ Data load done")
     
     # 전처리 하기
     preprocessor = Preprocessor(df_list)
     preprocessor.set_random_seed()
+    
+    
